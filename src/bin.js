b700fa8538681a7a505cf13fc4404f665af3a424
janus_wel <janus.wel.3@gmail.com>
2018-03-11 23:56:32 +0900

    :sparkles: can specify keyword from command line

--- src/bin.js
+++ src/bin.js
@@ -1,7 +1,37 @@
 // @flow
 
+import minimist from 'minimist'
 import lookup from './'
 
+const processArguments = () => {
+  const parsed = minimist(process.argv.slice(2))
+  const args = {
+    keyword: parsed._[0],
+  }
+
+  if (!args.keyword) {
+    throw new Error('specify a keyword to lookup dictionary')
+  }
+
+  if (parsed.service) {
+    args.service = parsed.service
+  }
+
+  return args
+}
+
 export default () => {
-  lookup('dictionary')
+  try {
+    const args = processArguments()
+    console.log(args)
+
+    if (args.service) {
+      lookup(args.keyword, args.service)
+      return
+    }
+
+    lookup(args.keyword)
+  } catch (e) {
+    console.error(e.message)
+  }
 }
