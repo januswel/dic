77a3e622be402728b28b2ffd1333ae6330d119a7
janus_wel <janus.wel.3@gmail.com>
2018-03-11 18:23:31 +0900

    :wrench: add flow

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // @flow
 
-import cheerio from 'cheerio'
+import $ from 'cheerio'
+import fetch from 'node-fetch'
+
+const DICTIONARIES = {
+    ALC: 'https://eow.alc.co.jp/search?q=${keyword}',
+}
+const DEFAULT = 'ALC'
+
+const main = async (keyword: string, site: string = DEFAULT) => {
+    const url = DICTIONARIES[site]
+    console.log(url)
+
+    const response = await fetch(url)
+    const html = await response.text()
+    console.log(html)
+
+    const dom = $(html)
+    console.log(dom)
+}
+
+main('dictionary')
