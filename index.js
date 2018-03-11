ee1c07a91856f53f0de51f6dd589682754e832b0
janus_wel <janus.wel.3@gmail.com>
2018-03-11 18:58:49 +0900

    :sparkles: implement roughly

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 // @flow
 
-import $ from 'cheerio'
+import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 
-const DICTIONARIES = {
-    ALC: 'https://eow.alc.co.jp/search?q=${keyword}',
+const SERVICES = {
+    ALC: {
+        TITLE: 'ALC 英辞郎',
+        URL: 'https://eow.alc.co.jp/search?q=${keyword}',
+        SELECTOR: '#resultsList li:has(.midashi)'
+    },
 }
-const DEFAULT = 'ALC'
+const DEFAULT = {
+    SERVICE: 'ALC',
+    NUMOF_DISPLAY: 10,
+}
 
-const main = async (keyword: string, site: string = DEFAULT) => {
-    const url = DICTIONARIES[site]
+
+const main = async (keyword: string, site: string = DEFAULT.SERVICE) => {
+    const service = SERVICES[site]
+    const urlTemplate = service.URL
+    const url = urlTemplate.replace('${keyword}', encodeURIComponent(keyword))
     console.log(url)
 
     const response = await fetch(url)
     const html = await response.text()
-    console.log(html)
+    // console.log(html)
 
-    const dom = $(html)
-    console.log(dom)
+    const $ = cheerio.load(html)
+
+    const matched = $(service.SELECTOR)
+    const converted = matched.map((i, el) => $(el).text()).get()
+    const result = converted.slice(0, DEFAULT.NUMOF_DISPLAY)
+    console.log(result)
 }
 
 main('dictionary')
