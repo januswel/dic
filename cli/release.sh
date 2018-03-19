50cb46b08bd23b374bbe0e22daf4680bfefd29e4
janus_wel <janus.wel.3@gmail.com>
2018-03-19 21:13:35 +0900

    :wrench: add settings to release by CI

--- cli/release.sh
+++ cli/release.sh
@@ -2,8 +2,5 @@
 
 yangpao -p
 git commit -am "release v$(yangpao -c)"
-git tag v$(yangpao -c)
+git tag "release cli v$(yangpao -c)"
 git push origin master --tags
-
-# TODO: setup CI
-npm publish --access public
