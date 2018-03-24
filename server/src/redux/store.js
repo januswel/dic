8596b36ef7c56fa25a3fd94d919f1caf4dd0a998
janus_wel <janus.wel.3@gmail.com>
2018-03-24 17:47:53 +0900

    :heavy_plus_sign: introduce redux-logger

--- server/src/redux/store.js
+++ server/src/redux/store.js
@@ -2,6 +2,7 @@
 
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
 
@@ -11,7 +12,7 @@ export const history = createHistory()
 
 const store = createStore(
   combineReducers({ routerReducer, authenticationReducer }),
-  applyMiddleware(routerMiddleware(history), thunk),
+  applyMiddleware(routerMiddleware(history), thunk, logger),
 )
 
 export default store
